559e1bdb386f90e0899ac8d668bd3c2774e08ad2
akashnarayane <akashnarayane@gmail.com>
2019-12-28 17:51:39 +0530

    Verizon SignIn Complete

---
+++ src/com/test/FlipCartCart.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FlipCartCart {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\37_40\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver chrome = new ChromeDriver();
+		
+		chrome.get("https://www.flipkart.com/");
+		Thread.sleep(4000);
+		
+		
+	}
+
+}
