1e84acf2d0df566e87d54e484fd3235356ef5fe3
akashnarayane <akashnarayane@gmail.com>
2019-12-28 18:03:46 +0530

    Branch1

--- src/com/test/FlipCartCart.java
+++ src/com/test/FlipCartCart.java
@@ -1,6 +1,8 @@
 package com.test;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class FlipCartCart {
 
@@ -12,8 +14,19 @@ public class FlipCartCart {
 		
 		chrome.get("https://www.flipkart.com/");
 		Thread.sleep(3000);
-		
-		
+		/*
+		 * WebElement signIn =
+		 * chrome.findElementByXPath("//*[@id=\"gnavAccountMenu\"]/span");
+		 * Thread.sleep(1000);
+		 * 
+		 * Actions mouseHover = new Actions(chrome);
+		 * mouseHover.moveToElement(signIn).build().perform();
+		 * 
+		 * 
+		 * chrome.findElementByXPath(
+		 * "//*[@id=\"vzw-gn\"]/div/nav/div/div[2]/div[3]/ul[2]/li[3]/ul/li[1]/a").click
+		 * ();
+		 */
 	}
 
 }
